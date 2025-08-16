eea35c41938340045f2e33a3edf9d959afaf46a1
xpcosmos <mikeias.d.s.o@gmail.com>
2025-08-16 14:18:03 -0300

    deps: Adicionado dependencias

---
+++ src/main/java/com/xpcosmos/transacoes_bancarias/TransacoesBancariasApplication.java
@@ -0,0 +1,13 @@
+package com.xpcosmos.transacoes_bancarias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransacoesBancariasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransacoesBancariasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xpcosmos/transacoes_bancarias/TransacoesBancariasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpcosmos.transacoes_bancarias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacoesBancariasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
