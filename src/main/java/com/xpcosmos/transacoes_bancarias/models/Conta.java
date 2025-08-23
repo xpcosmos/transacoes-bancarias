28ec8afc3daa0a3f4207cc414b049b489b7d23cf
xpcosmos <mikeias.d.s.o@gmail.com>
2025-08-23 19:24:28 -0300

    feat: adicionado decorators para colunas e adicao de constructor vazio

--- src/main/java/com/xpcosmos/transacoes_bancarias/models/Conta.java
+++ src/main/java/com/xpcosmos/transacoes_bancarias/models/Conta.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import com.xpcosmos.transacoes_bancarias.dto.ContaDTO;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,31 +14,37 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "conta")
 public class Conta {
-  
+
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
   private UUID Id;
+  @Column(unique = true, nullable = false, updatable = false)
   private String numeroDeConta;
+  @Column(unique = true, nullable = false, precision = 2, updatable = true)
   private Number saldo;
 
-  Conta(ContaDTO conta){
+  Conta() {
+  }
+
+  Conta(ContaDTO conta) {
     this.numeroDeConta = conta.numeroDeConta();
     this.saldo = conta.saldo();
   }
 
+  @Column(unique = true, nullable = false)
   public String getNumeroDeConta() {
     return this.numeroDeConta;
   }
 
-  public void setNumeroDeConta(String s){
+  public void setNumeroDeConta(String s) {
     this.numeroDeConta = s;
   }
 
-    public Number getSaldo() {
+  public Number getSaldo() {
     return this.saldo;
   }
 
-  public void setSaldo(Number n){
+  public void setSaldo(Number n) {
     this.saldo = n;
   }
 }
