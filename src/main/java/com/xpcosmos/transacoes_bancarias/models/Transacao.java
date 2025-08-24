f42f6ae8666b4212dde2656ae537f5272c1f821d
xpcosmos <mikeias.d.s.o@gmail.com>
2025-08-24 17:00:26 -0300

    feat: adicionado relacionamento entre transacao e conta no nome `Transacao`

--- src/main/java/com/xpcosmos/transacoes_bancarias/models/Transacao.java
+++ src/main/java/com/xpcosmos/transacoes_bancarias/models/Transacao.java
@@ -9,12 +9,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "transacoes")
+@Table(name = "transacao")
 public class Transacao {
-  
 
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
@@ -22,6 +23,10 @@ public class Transacao {
 
   @Column(nullable = false)
   StatusTransacionais status;
+
+  @ManyToOne
+  @JoinColumn(name = "conta_id")
+  Conta conta;
 
   Transacao(){
     this.status = StatusTransacionais.PROCESSANDO;
