d64dd32e25308a710bbfeada3915ebb66bc684a0
xpcosmos <mikeias.d.s.o@gmail.com>
2025-08-30 18:37:16 -0300

    test: Adicionado testes de Valores de transferencia negativas

--- src/test/java/com/xpcosmos/transacoes_bancarias/assets/UserTestResource.java
+++ src/test/java/com/xpcosmos/transacoes_bancarias/assets/UserTestResource.java
@@ -12,9 +12,21 @@ import com.xpcosmos.transacoes_bancarias.models.User.UserType;
 public class UserTestResource {
 	ObjectMapper mapper = new ObjectMapper();
 
+	public UserDTO gerarUserTestDTO(){return gerarUserTestDTO(Optional.empty(), Optional.empty());}
+	public UserDTO gerarUserTestDTO(UserType userType){return gerarUserTestDTO(Optional.of(userType),Optional.of(0f));}
+	public UserDTO gerarUserTestDTO(Float saldo){return gerarUserTestDTO(Optional.empty(),Optional.of(saldo));}
+
+
+	public User gerarUserTest(){return  new User(gerarUserTestDTO());}
+	public User gerarUserTest(UserType userType){return  new User(gerarUserTestDTO(userType));}
+	public User gerarUserTest(Float saldo){return  new User(gerarUserTestDTO(saldo));}
+
+	public String gerarUserTestDtoAsString() throws JsonProcessingException{
+		return mapper.writeValueAsString(gerarUserTestDTO());}
+
 	public UserDTO gerarUserTestDTO(Optional<UserType> userTypeOpt, Optional<Float> saldoOptional){
-		UserType userType = userTypeOpt == null ? UserType.COMUM : userTypeOpt.get();
-		Float saldo = saldoOptional == null ? 0f : saldoOptional.get();
+		UserType userType = userTypeOpt.orElse(UserType.COMUM);
+		Float saldo = saldoOptional.orElse(0f);
 		String randomString = String.valueOf(LocalDateTime.now().hashCode());
 		return  new UserDTO(
       "User Teste",
--- src/test/java/com/xpcosmos/transacoes_bancarias/services/TransferenciaServiceTest.java
+++ src/test/java/com/xpcosmos/transacoes_bancarias/services/TransferenciaServiceTest.java
@@ -1,6 +1,5 @@
 package com.xpcosmos.transacoes_bancarias.services;
 
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
@@ -12,6 +11,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.xpcosmos.transacoes_bancarias.assets.UserTestResource;
 import com.xpcosmos.transacoes_bancarias.dto.TransacaoDTO;
+import com.xpcosmos.transacoes_bancarias.exceptions.InvalidOperationException;
 import com.xpcosmos.transacoes_bancarias.exceptions.NotEnoughMoneyException;
 import com.xpcosmos.transacoes_bancarias.models.User.User;
 
@@ -25,10 +25,23 @@ public class TransferenciaServiceTest extends UserTestResource {
 
 	@Test
 	void testExceptionSaldoInsuficiente() throws Exception {
-		User user = gerarUserTest(null, null);
+		User user = gerarUserTest();
 		TransacaoDTO transacaoDTO = new TransacaoDTO(1l, user.getId(), 10f);
 		when(userService.getUserById(user.getId())).thenReturn(user);
 		assertThrowsExactly(NotEnoughMoneyException.class, () -> transferenciaService.tranferir(transacaoDTO));
+	}
+
+	@Test
+	void testExceptionValorNegativo() throws Exception {
+
+		User pagador = gerarUserTest(20f);
+		User beneficiario = gerarUserTest();
+		TransacaoDTO transacaoDTO = new TransacaoDTO(pagador.getId(), beneficiario.getId(), -10f);
+
+		when(userService.getUserById(pagador.getId())).thenReturn(pagador);
+		when(userService.getUserById(beneficiario.getId())).thenReturn(beneficiario);
+
+		assertThrowsExactly(InvalidOperationException.class, () -> transferenciaService.tranferir(transacaoDTO));
 
 	}
 }
