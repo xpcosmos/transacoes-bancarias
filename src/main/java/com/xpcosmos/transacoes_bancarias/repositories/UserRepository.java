b06108f91517e7a04591cc50937cfb4b18fec302
xpcosmos <mikeias.d.s.o@gmail.com>
2025-08-30 14:32:50 -0300

    feat: Modificado tipo de retorno de findByDocumentId para ser Optional

--- src/main/java/com/xpcosmos/transacoes_bancarias/repositories/UserRepository.java
+++ src/main/java/com/xpcosmos/transacoes_bancarias/repositories/UserRepository.java
@@ -1,13 +1,13 @@
 package com.xpcosmos.transacoes_bancarias.repositories;
 
-import java.util.UUID;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.xpcosmos.transacoes_bancarias.models.User.User;
 
 
-public interface UserRepository extends JpaRepository<User, UUID> {
-  User findByDocumentoId(String documentoId);
+public interface UserRepository extends JpaRepository<User, Long> {
+  Optional<User> findByDocumentoId(String documentoId);
   boolean existsByDocumentoId(String documentoId);
 }
