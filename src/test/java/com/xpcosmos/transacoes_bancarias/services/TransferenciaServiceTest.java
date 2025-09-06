f964b6b61b13bcb29efe50991e43504c64af255d
xpcosmos <mikeias.d.s.o@gmail.com>
2025-09-06 14:44:41 -0300

    test: Teste unitario de exception em caso de usuario lojista fazendo fazendo debitacao

--- src/test/java/com/xpcosmos/transacoes_bancarias/services/TransferenciaServiceTest.java
+++ src/test/java/com/xpcosmos/transacoes_bancarias/services/TransferenciaServiceTest.java
@@ -14,21 +14,22 @@ import com.xpcosmos.transacoes_bancarias.dto.TransacaoDTO;
 import com.xpcosmos.transacoes_bancarias.exceptions.InvalidOperationException;
 import com.xpcosmos.transacoes_bancarias.exceptions.NotEnoughMoneyException;
 import com.xpcosmos.transacoes_bancarias.models.User.User;
+import com.xpcosmos.transacoes_bancarias.models.User.UserType;
 
 import jakarta.transaction.Transactional;
 
 @ExtendWith(MockitoExtension.class)
 public class TransferenciaServiceTest extends UserTestResource {
 
-	@Mock
-	UserService userService;
-	@InjectMocks
-	TransferenciaService transferenciaService;
+  @Mock
+  UserService userService;
+  @InjectMocks
+  TransferenciaService transferenciaService;
 
-	@Test
+  @Test
   @Transactional
-	void testExceptionSaldoInsuficiente() throws Exception {
-		User pagador = gerarEntity();
+  void testExceptionSaldoInsuficiente() throws Exception {
+    User pagador = gerarEntity();
     User beneficiario = gerarEntity();
 
     Long pagadorId = pagador.getId();
@@ -36,22 +37,40 @@ public class TransferenciaServiceTest extends UserTestResource {
 
     when(userService.getUserById(pagadorId)).thenReturn(pagador);
     when(userService.getUserById(beneficiarioId)).thenReturn(beneficiario);
-		TransacaoDTO transacaoDTO = new TransacaoDTO(beneficiarioId, pagadorId, 10f);
-		assertThrowsExactly(NotEnoughMoneyException.class, () -> transferenciaService.tranferir(transacaoDTO));
-	}
+    TransacaoDTO transacaoDTO = new TransacaoDTO(beneficiarioId, pagadorId, 10f);
+    assertThrowsExactly(NotEnoughMoneyException.class, () -> transferenciaService.tranferir(transacaoDTO));
+  }
 
-	@Test
-	void testExceptionValorNegativo() throws Exception {
+  @Test
+  void testExceptionValorNegativo() throws Exception {
 
-		User pagador = gerarEntity();
-		pagador.setSaldo(20f);
-		User beneficiario = gerarEntity();
-		TransacaoDTO transacaoDTO = new TransacaoDTO(pagador.getId(), beneficiario.getId(), -10f);
+    User pagador = gerarEntity();
+    pagador.setSaldo(20f);
+    User beneficiario = gerarEntity();
+    TransacaoDTO transacaoDTO = new TransacaoDTO(pagador.getId(), beneficiario.getId(), -10f);
 
-		when(userService.getUserById(pagador.getId())).thenReturn(pagador);
-		when(userService.getUserById(beneficiario.getId())).thenReturn(beneficiario);
+    when(userService.getUserById(pagador.getId())).thenReturn(pagador);
+    when(userService.getUserById(beneficiario.getId())).thenReturn(beneficiario);
 
-		assertThrowsExactly(InvalidOperationException.class, () -> transferenciaService.tranferir(transacaoDTO));
+    assertThrowsExactly(InvalidOperationException.class, () -> transferenciaService.tranferir(transacaoDTO));
 
-	}
+  }
+
+  @Test
+  void testExceptionLogistaComoPagador() throws Exception {
+    User pagador = gerarEntity();
+    User beneficiario = gerarEntity();
+
+    Long pagadorId = pagador.getId();
+    Long beneficiarioId = beneficiario.getId();
+
+    pagador.setTipoUsuario(UserType.LOJISTA);
+
+    when(userService.getUserById(beneficiarioId)).thenReturn(beneficiario);
+    when(userService.getUserById(pagadorId)).thenReturn(pagador);
+
+    assertThrows(Exception.class, () -> transferenciaService.tranferir(
+        new TransacaoDTO(pagadorId, beneficiarioId, 10f)));
+
+  }
 }
