a5bbf2fc6f8f6524678b7e3175b857cdd7203a1a
xpcosmos <mikeias.d.s.o@gmail.com>
2025-09-06 11:33:18 -0300

    feature: adicionado classe para identificar nivels de permissoes

---
+++ src/main/java/com/xpcosmos/transacoes_bancarias/models/permissions/PermissionLevel.java
@@ -0,0 +1,8 @@
+package com.xpcosmos.transacoes_bancarias.models.permissions;
+
+public class PermissionLevel {
+  public Integer read = 0;
+  public Integer write = 1;
+  public Integer update = 2;
+  public Integer delete = 3;
+}
