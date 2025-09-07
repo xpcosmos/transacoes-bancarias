e46089ac18e946bea493d5373df4d3ea8a2a9bcf
xpcosmos <mikeias.d.s.o@gmail.com>
2025-09-07 16:17:57 -0300

    test: adicionado teste para post de notificacao

--- src/test/java/com/xpcosmos/transacoes_bancarias/services/external/NotificationServiceTest.java
+++ src/test/java/com/xpcosmos/transacoes_bancarias/services/external/NotificationServiceTest.java
@@ -1,10 +1,22 @@
 package com.xpcosmos.transacoes_bancarias.services.external;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class NotificationServiceTest {
+
+  @InjectMocks
+  NotificationService notificationService;
 
   @Test
   void testGetNotificationResponse() {
+    String response = notificationService.getNotificationResponse().block();
+    assertEquals("", response);
   }
 }
