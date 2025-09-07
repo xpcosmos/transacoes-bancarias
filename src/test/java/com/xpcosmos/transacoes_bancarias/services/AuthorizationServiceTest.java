dee30d3b4b66cd5c95cc3f2850d8ab2499ca46ac
xpcosmos <mikeias.d.s.o@gmail.com>
2025-09-07 12:19:51 -0300

    test: adicionado testes para chamada de servicos externos

--- src/test/java/com/xpcosmos/transacoes_bancarias/services/AuthorizationServiceTest.java
+++ src/test/java/com/xpcosmos/transacoes_bancarias/services/AuthorizationServiceTest.java
@@ -1,11 +1,18 @@
 package com.xpcosmos.transacoes_bancarias.services;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+
+import com.xpcosmos.transacoes_bancarias.dto.AuthorizationDTO;
+import com.xpcosmos.transacoes_bancarias.exceptions.ExternalServiceException;
 
 @ExtendWith(MockitoExtension.class)
 public class AuthorizationServiceTest {
@@ -14,7 +21,26 @@ public class AuthorizationServiceTest {
   AuthorizationService authorizationService;
 
   @Test
-  void testGetAuthorizationResponse() {
-    assertDoesNotThrow(() -> authorizationService.getAuthorizationResponse());
+  void testGetAuthorizationResponse() throws ExternalServiceException {
+    try {
+      var response = authorizationService.getAuthorizationResponse();
+      assertNotNull(response);
+      assertTrue(response.hasBody());
+    } catch (ExternalServiceException e) {
+      assertFalse(e.getMessage().isBlank());
+    }
+  }
+
+  @Test
+  void testResponseDataStructure() throws HttpClientErrorException {
+    try {
+      ResponseEntity<AuthorizationDTO> authorization = authorizationService.getAuthorizationResponse();
+      AuthorizationDTO reponse = authorization.getBody();
+      assertNotNull(reponse);
+      assertTrue(reponse.data().containsKey("authorization"));
+    } catch (ExternalServiceException e) {
+      assertFalse(e.getMessage().isBlank());
+    }
+
   }
 }
